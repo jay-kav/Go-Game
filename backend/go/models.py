8db2baead72968d7bc46e46768bc9ce7b3ab65a5
fernandobusta <bustafernando@icloud.com>
2023-11-09 13:56:29 +0000

    Models and admin setup

--- backend/go/models.py
+++ backend/go/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+# Every time a model is made:
+# python manage.py makemigrations
+# python manage.py migrate
+
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
