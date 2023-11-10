9bf13d0e9c0b512a46b39463bd95309f77c5d44c
fernandobusta <bustafernando@icloud.com>
2023-11-10 11:21:08 +0000

    Serializers file

--- backend/go/urls.py
+++ backend/go/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+# Create and define router
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="index"),
+   path('add', views.api_add, name="api_add"),
+   path('api/', include(router.urls)),
 ]
--- backend/go/views.py
+++ backend/go/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def api_add(request):
+    num1 = float(request.GET.get('num1', 1))
+    num2 = float(request.GET.get('num2', 1))
+    added = num1 + num2
+    resp_dict = {'result': added}
+    
+    return JsonResponse(resp_dict)
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
